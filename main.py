6f6a9b942d1edd409a32e1a62c54b94d02438c7b
Neeraj Gartia <neerajgartia211002@gmail.com>
2023-02-01 21:04:01 +0530

    added cors

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
